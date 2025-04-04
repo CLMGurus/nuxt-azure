d850570908e751b11b3c4beac91935b3b597786d
balasakthi <balasakthi.g@gmail.com>
2025-04-04 14:16:31 +0530

    removed docker

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
-  nitro: {
-    preset: "node-server",
-  },
+  // nitro: {
+  //   preset: "node-server",
+  // },
   devServer: {
     port: 8080,
     host: "0.0.0.0",
